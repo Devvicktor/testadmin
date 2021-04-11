c82ff758e1618e48bd5266cb40cce6f378b373ce
Devvicktor <vkosgei001@gmail.com>
2021-04-11 16:25:47 +0300

    server

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = process.env.port || 3000;
+
 app.use(express.static(__dirname + '/build'));
 app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => {
+    console.log(`listening on port ${port} ...... `);
+});
